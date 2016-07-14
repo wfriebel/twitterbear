ba0d277f8d91ad5109daed333bed731a116f905f
Will Friebel <will.friebel@gmail.com>
2016-07-14 16:30:53 -0700

    create model and migrate

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,3 +1,5 @@
 class Follower < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :follower, foreign_key: "follower_id", class_name: "User"
+  belongs_to :followee, foreign_key: "followee_id", class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
-  has_many :followers
+  has_many :followers, foreign_key: "followee_id", class_name: "Follower"
+  has_many :followees, through: :followers
+  has_many :tweets
 end
