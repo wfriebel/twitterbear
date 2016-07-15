0e63635cd03eaeb1ec28ab3dabbe55c4ee169e5d
Shawn Tuttle <email": null>
2016-07-14 21:10:36 -0700

    Correct delete table names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 require 'faker'
 
-Article.delete_all
-Category.delete_all
+User.delete_all
+Tweet.delete_all
 
 500.times do
   User.create(:first_name => Faker::Name.first_name, :last_name => Faker::Name.last_name, :user_name => Faker::Internet.user_name, :email => Faker::Internet.email, :password_hash => Faker::Internet.password, :image_url => Faker::Internet.url)
