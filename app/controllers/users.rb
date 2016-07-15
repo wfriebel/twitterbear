75fff2a003c40d9711692018928394286fb3bad4
Renan Barbosa e Souza & Shawn Tuttle <pair+renanba.shawnte@devbootcamp.com>
2016-07-15 15:40:10 -0700

    Add tweet form to profile page and save into Tweet table

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -7,3 +7,11 @@ get '/users/:id' do
   @user = User.find_user_by_id(params[:id])
   erb :"/users/show"
 end
+
+post '/users/:id/tweet' do
+	p "before save"
+  @tweet = Tweet.create(tweet: params[:tweet], user_id: params[:id])
+  p @tweet.user_id
+  redirect "/users/#{@tweet.user_id}"
+  p "after redirect"
+end
