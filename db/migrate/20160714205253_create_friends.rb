5d93428f9228a50971635791690887780b76aca0
Will Friebel <will.friebel@gmail.com>
2016-07-15 11:47:25 -0700

    Database associations all work

---
+++ db/migrate/20160714205253_create_friends.rb
@@ -0,0 +1,10 @@
+class CreateFriends < ActiveRecord::Migration
+  def change
+  	create_table :friends do |t|
+ 		t.integer :follower_id
+ 		t.integer :followee_id
+
+ 		t.timestamps 		
+  	end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+require 'faker'
+
+#Destroy all existing users and tweets
+users = User.all
+users.each {|user| user.destroy}
+
+tweets = Tweet.all
+tweets.each {|tweet| tweet.destroy}
+
+friends = Friend.all
+friends.each {|friend| friend.destroy}
+#Create 100 users
+100.times do
+	User.create(
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		user_name: Faker::Internet.user_name,
+		email: Faker::Internet.email,
+		password_hash: Faker::Internet.password,
+		image_url: Faker::Company.logo
+	)
+
+#Create 100 tweets
+	Tweet.create(
+		tweet: Faker::Lorem.characters(120)
+	)
+end
