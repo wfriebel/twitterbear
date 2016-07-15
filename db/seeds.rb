01936470fb00b972f11644885012455a6dc1a1cd
shawn <shawn@naturalprofessional.com>
2016-07-14 20:50:27 -0700

    Add Faker seed data

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+
+require 'faker'
+
+Article.delete_all
+Category.delete_all
+
+500.times do
+  User.create(:first_name => Faker::Name.first_name, :last_name => Faker::Name.last_name, :user_name => Faker::Internet.user_name, :email => Faker::Internet.email, :password_hash => Faker::Internet.password, :image_url => Faker::Internet.url)
+end
+
+2000.times do
+  Tweet.create(:tweet => Faker::Lorem.characters(10..140), :user_id => rand(1..500))
+end
